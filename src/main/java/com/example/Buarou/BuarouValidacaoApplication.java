d2dd6d369bee5317de267740292fdf5142e76ab2
actdigital <carlos.loureiro@actdigital.com>
2024-05-07 18:34:14 -0300

    retirada da chamada ao servilo externo

--- src/main/java/com/example/Buarou/BuarouValidacaoApplication.java
+++ src/main/java/com/example/Buarou/BuarouValidacaoApplication.java
@@ -2,10 +2,9 @@ package com.example.Buarou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @SpringBootApplication
-@EnableFeignClients
 public class BuarouValidacaoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/Buarou/adapters/out/ValidarEnderecoAdapter.java
+++ src/main/java/com/example/Buarou/adapters/out/ValidarEnderecoAdapter.java
@@ -1,6 +1,5 @@
 package com.example.Buarou.adapters.out;
 
-import com.example.Buarou.adapters.out.client.OutroServicoClient;
 import com.example.Buarou.application.core.domain.PessoaDomain;
 import com.example.Buarou.application.ports.OutputPorts.ValidadorEnderecoPessoaOutPutPort;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ValidarEnderecoAdapter implements ValidadorEnderecoPessoaOutPutPort {
-    private final OutroServicoClient outroServicoClient;
+
     @Autowired
-    public ValidarEnderecoAdapter(OutroServicoClient outroServicoClient) {
-        this.outroServicoClient = outroServicoClient;
+    public ValidarEnderecoAdapter() {
+
     }
 
     @Override
     public void validadorDeEndereco(PessoaDomain pessoaDomain) {
-        outroServicoClient.obterDados(pessoaDomain);
     }
 }
